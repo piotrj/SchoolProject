858b11e35db79b67e3f06d2376dfeb6d3ae8746e
Piotr Jakubowski <piotrj@piotr-jakubowskis-macbook.local>
2009-11-23 11:29:00 +0100

    "Config"

--- config/environment.rb
+++ config/environment.rb
@@ -15,9 +15,9 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "mysql"
   config.gem "rspec", :version => ">= 1.2.8", :lib => false
   config.gem "rspec-rails", :lib => false
+  config.gem "authlogic", :version => ">= 2.1.2"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
