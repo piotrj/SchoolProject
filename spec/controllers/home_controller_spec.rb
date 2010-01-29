cbbd977fd596e9b275670f874a09fb39aba2537c
Piotr Jakubowski <piotrj@gmail.com>
2010-01-29 09:22:18 +0100

    issue #4 - few tests fixes

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,6 +1,21 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe HomeController do
+  before do
+    @user = Factory(:user)
+  end
   
+  describe "GET index" do
+    it "should be success if user is not logged in" do
+      get :index
+      response.should be_success
+    end
+    
+    it "should redirect if user is logged in" do
+      controller.stub!(:current_user).and_return(@user)
+      get :index
+      response.should redirect_to(school_tests_path)
+    end
+  end
 
 end
--- spec/controllers/school_tests_controller_spec.rb
+++ spec/controllers/school_tests_controller_spec.rb
@@ -2,24 +2,36 @@ require 'spec_helper'
 
 describe SchoolTestsController do
   
-  # #Delete this example and add some real ones
-  # it "should use SchoolTestsController" do
-  #   controller.should be_an_instance_of(SchoolTestsController)
-  # end
-  
-  before do
-    @user = Factory.create(:user)
-  end
-
-  describe "GET index" do
-    it "should not allow to access if user is not logged in" do
+  describe "if not logged in" do 
+    it "GET index should not allow to access if user is not logged in" do
       lambda { get :index}.should raise_error("Unauthorized access")
     end
     
-    it "should allow to acces if user is logged in" do
+    it "GET new should not allow to access if user is not logged in" do
+      lambda { get :new}.should raise_error("Unauthorized access")
+    end
+  end
+
+  describe "if logged in" do
+    before do
+      @user = Factory.create(:user)
       controller.stub!(:current_user).and_return(@user)
-      get :index
-      response.should be_success
+    end
+    
+    describe "GET index" do
+    
+      it "should be success" do
+        get :index
+        response.should be_success
+      end
+    end
+  
+    describe "GET new" do
+    
+      it "should be success" do
+        get :new
+        response.should be_success
+      end
     end
   end
 end
