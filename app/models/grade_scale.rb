b61281e55de49dae3f08a5ebf23ae8de3b0dd8aa
Piotr Kochowicz <hesler@hesler.(none)>
2010-02-03 10:11:27 +0100

    poprawiona skala

--- app/models/grade_scale.rb
+++ app/models/grade_scale.rb
@@ -5,31 +5,23 @@ class GradeScale < ActiveRecord::Base
 	validates_inclusion_of :four, :in => 0..100, :message => "Proszę podać liczbę od 0 do 100."
 	validates_inclusion_of :three_and_half, :in => 0..100, :message => "Proszę podać liczbę od 0 do 100."
 	validates_inclusion_of :three, :in => 0..100, :message => "Proszę podać liczbę od 0 do 100."
-	validate :five_validator
-	validate :four_and_half_validator
-	validate :four_validator
-	validate :three_and_half_validator
+	validate :grade_validator
 	
-	def five_validator
-		if (self.four_and_half > self.five)
-			self.errors.add_to_base("Niepoprawna skala")
-		end
-	end
-	
-	def four_and_half_validator
-		if (self.four > self.four_and_half)
-			self.errors.add_to_base("Niepoprawna skala")
-		end
-	end
-	
-	def four_validator
-		if (self.three_and_half > self.four)
-			self.errors.add_to_base("Niepoprawna skala")
-		end
-	end
-	
-	def three_and_half_validator
-		if (self.three > self.three_and_half)
+	def grade_validator
+		if self.five && self.four_and_half && self.four && self.three_and_half && self.three
+			if (self.four_and_half > self.five)
+				self.errors.add_to_base("Niepoprawna skala")
+			end	
+			if (self.four > self.four_and_half)
+				self.errors.add_to_base("Niepoprawna skala")
+			end	
+			if (self.three_and_half > self.four)
+				self.errors.add_to_base("Niepoprawna skala")
+			end
+			if (self.three > self.three_and_half)
+				self.errors.add_to_base("Niepoprawna skala")
+			end
+		else
 			self.errors.add_to_base("Niepoprawna skala")
 		end
 	end
